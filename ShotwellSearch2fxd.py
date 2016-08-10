b009f1fcefd40daf00f011dc72d1fe761652d35f
pablo33 <pcasas33@gmail.com>
2016-08-10 21:32:04 +0200

    limited to 1000 items the generation of the fxd files

--- ShotwellSearch2fxd.py
+++ ShotwellSearch2fxd.py
@@ -3,7 +3,7 @@
 import os, shutil
 from ShotwellSearchAPI import ShotwellSearch
 
-freevofxduserpath = os.path.join(os.getenv('HOME'),".freevo/fxd/photos/")
+freevofxduserpath = os.path.join(os.getenv('HOME'),".freevo/fxd/Shotwellfilters/")
 
 def writesound (f, soundfolderpath = "", ramdom = 0, recursive = 0):
 	f.write ('\t\t<background-music random="%s">\n'%ramdom)
@@ -18,8 +18,11 @@ def writecover (f, coverfilepath = ""):
 def writefilesentries (f, SR, duration = 0):
 	""" Writes a block of files-entries in an fxd file """
 	f.write ("\t\t<files>\n")
+	count = 0
 	for Id, entry in SR.Resultentries():
 		f.write('\t\t\t<file duration="%(duration)s">%(file)s</file>\n'%{'duration': duration, 'file': entry})
+		count += 1
+		if count > 1000: break
 	f.write ("\t\t</files>\n")
 	return
 
@@ -27,7 +30,7 @@ def	writeslideshowblock (f, SR, ramdom = 0, repeat = 0):
 	""" Writes a slideshow fxd block """
 	f.write('\t<slideshow title="%(title)s" random="%(ramdom)s" repeat="%(repeat)s">\n'%{'title':SR.searchname, 'ramdom':ramdom, 'repeat': repeat})
 	writecover (f)
-	writesound (f)
+	#writesound (f)
 	writefilesentries (f, SR)
 	f.write('\t</slideshow>\n')
 	return
@@ -41,13 +44,20 @@ def Writefxd (SR, folderpath):
 	writeslideshowblock (f, SR)
 	f.write('</freevo>')
 	f.close()
-	return
+	return SR.searchname ,fxdfilename
 
-if __name__ == '__main__':
+def generatefxd (freevofxduserpath):
 	SR = ShotwellSearch()
+	fxdlist = []
+	if os.path.isdir(freevofxduserpath):
+		shutil.rmtree(freevofxduserpath)
+	os.makedirs (freevofxduserpath)
+
 	for SearchId, searchname, foo in SR.Searchtable():
 		SR.Search(SearchId)
-		Writefxd (SR, freevofxduserpath)
+		fxdlist.append (Writefxd (SR, freevofxduserpath))
+	return fxdlist
 
-
-
+if __name__ == '__main__':
+	generatedlist = generatefxd (freevofxduserpath)
+	print (len (generatedlist), "fxd files has been created at %s"%freevofxduserpath)
